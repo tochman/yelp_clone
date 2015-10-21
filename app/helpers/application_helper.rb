f8dca3eba4b6a13c80c680e3617fb88244284fb5
tochman <thomas@nocebo.se>
2015-10-21 17:35:00 +0200

    WIP added gravatar method for current_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
   def user_avatar
-    'http://www.websitemagazine.com/images/blog/anonymous-user.png'
+    gravatar_id = Digest::MD5::hexdigest(current_user.email) if current_user
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    res = Net::HTTP.get_response(URI("#{gravatar_url}?d=404"))
+    if res.code == '404'
+      'http://www.websitemagazine.com/images/blog/anonymous-user.png'
+    else
+      gravatar_url
+    end
   end
 end
