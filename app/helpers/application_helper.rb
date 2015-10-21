de03bd4cd16f5a5bea758d61e488b799c00712b5
tochman <thomas@nocebo.se>
2015-10-21 23:28:17 +0200

    small refactoring of application helper#user_avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,6 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(current_user.email) if current_user
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     res = Net::HTTP.get_response(URI("#{gravatar_url}?d=404"))
-    if res.code == '404'
-      'http://www.websitemagazine.com/images/blog/anonymous-user.png'
-    else
-      gravatar_url
-    end
+    (res.code == '404') ? 'http://www.websitemagazine.com/images/blog/anonymous-user.png' : gravatar_url
   end
 end
