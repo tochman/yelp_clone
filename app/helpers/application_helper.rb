21f476be3e05673a142a39227aca8220888aedcf
tochman <thomas@nocebo.se>
2015-10-22 11:20:15 +0200

    refactored application_helper#user_avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def user_avatar
-    gravatar_id = Digest::MD5::hexdigest(current_user.email) if current_user
+  def user_avatar(object)
+    gravatar_id = Digest::MD5::hexdigest(object.user.email) if object.user
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     res = Net::HTTP.get_response(URI("#{gravatar_url}?d=404"))
     (res.code == '404') ? 'http://www.websitemagazine.com/images/blog/anonymous-user.png' : gravatar_url
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,8 @@
 class Restaurant < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   belongs_to :user
+
+  after_create do
+    self.image_url = nil if self.image_url.empty?
+  end
 end
