76367c82d83c7176cf956ea540d790fe174b07a6
tochman <thomas@nocebo.se>
2015-10-23 13:16:17 +0200

    working on auth method on User

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,14 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      unless user
+        user.username = auth.info.nickname
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+        user.save!
+      end
     end
   end
 
