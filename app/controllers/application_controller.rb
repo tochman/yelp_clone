069e6b6216edd5dc449bfe67580d70add59f071e
tochman <thomas@nocebo.se>
2015-10-23 12:52:44 +0200

    backing out, choosing a simple path without helper methods ;-)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,16 @@ class ApplicationController < ActionController::Base
 
   helper_method :resource_name, :resource, :devise_mapping
 
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
